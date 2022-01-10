b819ab30fb71a2176027e06e7589450e8bd8d021
gyuyoung <ifos2u@naver.com>
2022-01-10 22:02:29 +0900

    220110

---
+++ Book-Flask-Backend-220107/app.py
@@ -0,0 +1,87 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+app.id_count = 1
+app.users = {}  # users 테이블
+app.tweets = []  # tweets 테이블
+
+
+@app.route('/sign-up', methods=['POST'])
+def sign_up():
+    payload = request.json
+    new_user = {
+        'id': app.id_count,
+        'name': payload.get('name'),
+        'email': payload.get('email'),
+        'hashed_password': payload.get('password'),
+        'profile': payload.get('profile')
+    }
+
+    app.users[app.id_count] = new_user
+    app.id_count += 1
+
+    return jsonify(new_user)
+
+
+@app.route('/tweet', methods=['POST'])
+def tweet():
+    payload = request.json
+    user_id = int(payload.get('id'))
+    tweet = payload.get('tweet')
+
+    if len(tweet) > 10:
+        return '10 over', 400
+
+    new_tweet = {
+        'user_id': user_id,
+        'tweet': tweet
+    }
+
+    app.tweets.append(new_tweet)
+    return jsonify(app.tweets)
+
+
+@app.route('/follow', methods=['POST'])
+def follow():
+    payload = request.json
+    user_id = int(payload.get('id'))
+    user_id_to_follow = int(payload.get('follow'))
+
+    if user_id not in app.users:
+        return 'no user', 400
+
+    user = app.users.get(user_id)
+    follow_list = user.setdefault('follow_list', [])
+    if user_id_to_follow not in follow_list:
+        follow_list.append(user_id_to_follow)
+
+    return jsonify(user)
+
+
+@app.route('/unfollow', methods=['GET'])
+def unfollow():
+    payload = request.json
+    user_id = int(payload.get('id'))
+    user_id_to_unfollow = int(payload.get('unfollow'))
+
+    if user_id not in app.users:
+        return 'no user', 400
+
+    user = app.users.get(user_id)
+    follow_list = user.setdefault('follow_list', [])
+    if user_id_to_unfollow in follow_list:
+        follow_list.remove(user_id_to_unfollow)
+
+    return jsonify(user)
+
+
+@app.route('/timeline/<int:user_id>', methods=['GET'])
+def timeline(user_id):
+    user = app.users.get(user_id)
+    target_list = user.get('follow_list', []) + [user_id]
+    timeline = [tweet for tweet in app.tweets if tweet.get('user_id') in target_list]
+
+    return jsonify({
+        'user_id': user_id,
+        'timeline': timeline
+    })
---
+++ Book-Flask-Backend-220107/test.py
@@ -0,0 +1,3 @@
+obj = set()
+print(type(obj) == set, type(obj) == list)
+print(isinstance(obj, set), isinstance(obj, list))
