814ebfa6f36f2e9102a6b5120c42357c9687dedb
gyuyoung <ifos2u@naver.com>
2022-02-02 22:07:54 +0900

    220202

--- Book-Flask-Backend-220107/chapter09/setup.py
+++ Book-Flask-Backend-220107/chapter09/setup.py
@@ -1,15 +1,17 @@
+# setup.py
+
 import sys
 from flask_script import Manager
 from flask_twisted import Twisted
 from twisted.python import log
 from app import create_app
 
-if __name__ == 'main':
+
+if __name__ == '__main__':
     app = create_app()
     twisted = Twisted(app)
     log.startLogging(sys.stdout)
 
     app.logger.info('Running the app...')
-
     manager = Manager(app)
     manager.run()
