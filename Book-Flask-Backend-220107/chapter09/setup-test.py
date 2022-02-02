f83158b10dd5cf522ae0bef5c549d36a0f97ec5e
gyuyoung <ifos2u@naver.com>
2022-02-02 21:25:42 +0900

    220202

--- Book-Flask-Backend-220107/chapter09/setup-test.py
+++ Book-Flask-Backend-220107/chapter09/setup-test.py
@@ -1,4 +1,9 @@
+import sys
+
 from flask import Flask
+from flask_script import Manager
+from flask_twisted import Twisted
+from twisted.python import log
 
 app = Flask(__name__)
 
@@ -10,8 +15,15 @@ def hello():
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    twisted = Twisted(app)
+    log.startLogging(sys.stdout)
+
+    app.logger.info('Running the app...')
+    manager = Manager(app)
+    manager.run()
 
 
-# FLASK_APP=setup.py flask run -h "0.0.0.0" -p 5000
-# python setup.py
+# FLASK_APP=setup-test.py flask run -h "0.0.0.0" -p 5000
+# python setup-test.py
+# python setup-test.py runserver -h "0.0.0.0" -p 5000 (flask_script)
+
